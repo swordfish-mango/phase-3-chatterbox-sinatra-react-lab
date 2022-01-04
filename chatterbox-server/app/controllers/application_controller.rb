476a9459080eddc6c7dfcd6f71f73044d30db6fd
Marcus <marcus.shimotsu@gmail.com>
2022-01-04 16:34:05 -0600

    commit tree 9a5ba44d7e2448943af856453dc5e1b601a51331

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,30 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
-  
+  get '/messages' do
+    Message.all.order(created_at: :asc).to_json
+  end
+
+  post '/messages' do
+    new_message = Message.create(
+      username: params[:username],
+      body: params[:body]
+    )
+    new_message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
+
 end
